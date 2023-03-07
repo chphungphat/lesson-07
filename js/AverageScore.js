ed6b3c144918133d79881b4e555f380cfb530364
Chung Phung Phat <chphungphat@gmail.com>
2023-03-07 15:10:42 +0700

    no

---
+++ js/AverageScore.js
@@ -0,0 +1,5 @@
+let Physics = parseInt(prompt('Nhap diem Ly'));
+let Chemical = parseInt(prompt('Nhap diem Hoa'));
+let Biology = parseInt(prompt('Nhap diem Sinh'));
+let average = (Physics+Chemical+Biology)/3;
+document.write('Diem trung binh = ' + average);
