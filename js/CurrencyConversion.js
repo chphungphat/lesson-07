f11b5dea1a237aa45089bca8cee69ee3ae4ea6d9
Chung Phung Phat <chphungphat@gmail.com>
2023-03-07 17:00:18 +0700

    no

---
+++ js/CurrencyConversion.js
@@ -0,0 +1,15 @@
+document.getElementById("result").innerHTML= +a;
+var a = document.getElementById('amount').value;
+
+
+
+
+function Conversion(a, b, c) {
+    
+    var a = document.getElementById('amount').value;
+    var b = document.getElementById('InputCurrency').value;
+    var c = document.getElementById('OutputCurrency').value;
+    document.getElementById("result").innerHTML= + a*250000;
+}
+        
+        
