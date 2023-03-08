c4b912639553e1ee15599eb81e901b754343d04c
Chung Phung Phat <chphungphat@gmail.com>
2023-03-08 09:45:04 +0700

    m

--- js/CurrencyConversion.js
+++ js/CurrencyConversion.js
@@ -1,15 +1,40 @@
-document.getElementById("result").innerHTML= +a;
-var a = document.getElementById('amount').value;
+function Conversion() {
+    const ratio = 25000;
+    var inamount = document.getElementById("amount").value;
+    var incur = document.getElementById("inputCurrency").value;
+    var outcur = document.getElementById("outputCurrency").value;
+    if (incur == "VND" && outcur == "USD")
+    {
+        let result = inamount/ratio;
+        document.getElementById("result").innerHTML = result;
+    }
+    else {
+        let result = inamount*ratio;
+        document.getElementById("result").innerHTML = result;
+    }
 
-
-
-
-function Conversion(a, b, c) {
-    
-    var a = document.getElementById('amount').value;
-    var b = document.getElementById('InputCurrency').value;
-    var c = document.getElementById('OutputCurrency').value;
-    document.getElementById("result").innerHTML= + a*250000;
-}
-        
-        
+    // switch(incur) {
+    //     case "USD":{
+    //         if (outcur == "VND") {
+    //             let result = inamount*ratio;
+    //             document.getElementById("result").innerHTML = "Kết quả: " + result + "VND";
+    //             break;
+    //         }
+    //         else {
+    //             document.getElementById("result").innerHTML = "Kết quả: " + inamount + "USD";
+    //             break;
+    //         }
+    //     }
+    //     case "VND": {
+    //         if (outcur == "USD") {
+    //             let result = inamount/ratio;
+    //             document.getElementById("result").innerHTML = "Kết quả: " + result + "USD";
+    //             break;
+    //         }
+    //         else {
+    //             document.getElementById("result").innerHTML = "Kết quả: " + inamount + "VND";
+    //             break;
+    //         }
+    //     }
+    // }
+}     
