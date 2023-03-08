a8c327e95b4be6ee95e9eb89705ee69193a30ce9
Chung Phung Phat <chphungphat@gmail.com>
2023-03-08 14:12:16 +0700

    add

--- js/CurrencyConversion.js
+++ js/CurrencyConversion.js
@@ -12,29 +12,4 @@ function Conversion() {
         let result = inamount*ratio;
         document.getElementById("result").innerHTML = result;
     }
-
-    // switch(incur) {
-    //     case "USD":{
-    //         if (outcur == "VND") {
-    //             let result = inamount*ratio;
-    //             document.getElementById("result").innerHTML = "Kết quả: " + result + "VND";
-    //             break;
-    //         }
-    //         else {
-    //             document.getElementById("result").innerHTML = "Kết quả: " + inamount + "USD";
-    //             break;
-    //         }
-    //     }
-    //     case "VND": {
-    //         if (outcur == "USD") {
-    //             let result = inamount/ratio;
-    //             document.getElementById("result").innerHTML = "Kết quả: " + result + "USD";
-    //             break;
-    //         }
-    //         else {
-    //             document.getElementById("result").innerHTML = "Kết quả: " + inamount + "VND";
-    //             break;
-    //         }
-    //     }
-    // }
 }     
